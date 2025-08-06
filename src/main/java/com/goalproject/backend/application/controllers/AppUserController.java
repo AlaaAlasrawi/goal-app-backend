2820537430830f0a8a359d69b6d623e5f79f49a5
AlaaAlasrawi <loloyousef2622003@gmail.com>
2025-08-06 10:39:57 +0300

    connect database

--- src/main/java/com/goalproject/backend/application/controllers/AppUserController.java
+++ src/main/java/com/goalproject/backend/application/controllers/AppUserController.java
@@ -4,7 +4,6 @@ import com.goalproject.backend.domain.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
