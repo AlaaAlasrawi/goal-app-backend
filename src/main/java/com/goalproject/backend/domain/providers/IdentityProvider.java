ad6b9afc193ac3460d42f7f91cd67159cfaf96a1
AlaaAlasrawi <loloyousef2622003@gmail.com>
2025-08-18 15:39:36 +0300

    init

--- src/main/java/com/goalproject/backend/domain/providers/IdentityProvider.java
+++ src/main/java/com/goalproject/backend/domain/providers/IdentityProvider.java
@@ -1,9 +1,13 @@
 package com.goalproject.backend.domain.providers;
 
 import com.goalproject.backend.domain.model.AppUser;
+import com.goalproject.backend.domain.service.security.AppUserDetailsService;
 import com.goalproject.backend.persistence.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -11,13 +15,36 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class IdentityProvider {
     private final AppUserRepository appUserRepository;
+    private final AppUserDetailsService appUserDetailsService;
 
     public AppUser currentIdentity() {
-        return appUserRepository.getById(1L);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.isAuthenticated()) {
+            if(authentication.getPrincipal() instanceof UserDetails userDetails) {
+                return appUserRepository.findByUsername(userDetails.getUsername());
+            }
+
+            if(authentication.getPrincipal() instanceof String username) {
+                return appUserRepository.findByUsername(username);
+            }
+        }
+
+        return null;
     }
 
-    public Long currentId() {
-        return 1L;
+    public UserDetails currentIdentityDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.isAuthenticated()) {
+            if(authentication.getPrincipal() instanceof UserDetails userDetails) {
+                return userDetails;
+            }
+
+            if(authentication.getPrincipal() instanceof String username) {
+                return appUserDetailsService.loadUserByUsername(username);
+            }
+        }
+
+        return null;
     }
 
 }
